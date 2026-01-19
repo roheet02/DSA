e330e35bf232ec9bb4b79abf078a78eb6338d8be
roheet02 <rohitfuke195@gmail.com>
2026-01-19 22:39:27 +0530

    Adding solution for Maximum Side Length of a Square with Sum Less than or Equal to Threshold

---
+++ codes/Graph/max_size_len_of_sq_with_sum_less_than_equal_to_threshold.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maxSideLength(self, mat, threshold):
+        m, n = len(mat), len(mat[0])
+
+        pref = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                pref[i][j] = (
+                    mat[i-1][j-1]
+                    + pref[i-1][j]
+                    + pref[i][j-1]
+                    - pref[i-1][j-1]
+                )
+
+        maxSide = min(m, n)
+
+        while maxSide > 0:
+            for i in range(m - maxSide + 1):
+                for j in range(n - maxSide + 1):
+                    s = (
+                        pref[i+maxSide][j+maxSide]
+                        - pref[i][j+maxSide]
+                        - pref[i+maxSide][j]
+                        + pref[i][j]
+                    )
+                    if s <= threshold:
+                        return maxSide
+            maxSide -= 1
+
+        return 0
+print(Solution().maxSideLength([[1,1,3,2,4,3,2],[1,1,3,2,4,3,2],[1,1,3,2,4,3,2]],4))
